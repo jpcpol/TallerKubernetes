59bb3b56f74484671ded733f7c4dbc9f850041ee
Juan Pablo Chancay <102939810+jpcpol@users.noreply.github.com>
2022-10-02 14:34:37 -0300

    Cambio de Ip

--- frontend/src/helpers/fetch.js
+++ frontend/src/helpers/fetch.js
@@ -1,5 +1,5 @@
 const fetchManage = ( endpoint, body, method = 'GET' ) => {
-    const url = 'http://34.151.254.33/' + endpoint;
+    const url = 'http://35.198.28.82/' + endpoint;
 
     if ( method === 'GET' ) {
         return fetch( url );
